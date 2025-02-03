d977f338710ef59b7f96b7f10945844817e9c264
Tokydaxxx <rtokyfitiavana@gmail.com>
2025-02-03 15:03:21 +0100

    Update insert.sql

---
+++ sql/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO elevage_animal(image, nom, poids_actuel, date_achat, est_vivant, utilisateur_id, type_animal_id) VALUES
+('assets/images/vache1.jpg', 'Vache Tachetée', 45.5, '2025-01-01', TRUE, 1, 1),
+('assets/images/vache2.jpg', 'Vache Brune', 47.0, '2025-01-02', TRUE, 1, 1),
+('assets/images/cochon1.jpg', 'Cochon Gris', 22.1, '2025-01-03', TRUE, 1, 2),
+('assets/images/cochon2.jpg', 'Cochon Rose', 24.5, '2025-01-04', TRUE, 1, 2),
+('assets/images/lapin1.jpg', 'Lapin Beige', 1.8, '2025-01-05', TRUE, 1, 3),
+('assets/images/lapin2.jpg', 'Lapin Blanc', 2.0, '2025-01-06', TRUE, 1, 3),
+('assets/images/mouton1.jpg', 'Mouton Blanc', 20.5, '2025-01-07', TRUE, 1, 4),
+('assets/images/mouton2.jpg', 'Mouton Marron', 18.3, '2025-01-08', TRUE, 1, 4),
+('assets/images/cheval1.jpg', 'Cheval Noir', 35.2, '2025-01-09', TRUE, 1, 5),
+('assets/images/cheval2.jpg', 'Cheval Blanc', 37.8, '2025-01-10', TRUE, 1, 5),
+('assets/images/poule1.jpg', 'Poule Rousse', 2.3, '2025-01-11', TRUE, 1, 6),
+('assets/images/poule2.jpg', 'Poule Blanche', 2.8, '2025-01-12', TRUE, 1, 6);
