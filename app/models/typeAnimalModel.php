70ac547d7f8e59cf5ecbd0bcb0114e44f197ba2d
NyEjaBoum <razanatsimbanyeja@gmail.com>
2025-02-03 20:45:22 +0300

    tt
    f

--- app/models/typeAnimalModel.php
+++ app/models/typeAnimalModel.php
@@ -17,8 +17,26 @@ class typeAnimalModel {
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function  createType(){
-        
+    public function createType($nom,$poidsMin,$poidsMax,$prixVenteKg,$jour,$pourcentage){
+        $sql = "INSERT INTO elevage_typeAnimal (nom,poids_minimal_vente,poids_maximal,prix_vente_kg,jours_sans_manger,pourcentage_perte_poids)
+        VALUES (?,?,?,?,?,?)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$nom,$poidsMin,$poidsMax,$prixVenteKg,$jour,$pourcentage]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function updateType($id, $nom, $poidsMin, $poidsMax, $prixVenteKg, $jour, $pourcentage) {
+        $sql = "UPDATE elevage_typeAnimal 
+                SET nom = ?, 
+                    poids_minimal_vente = ?, 
+                    poids_maximal = ?, 
+                    prix_vente_kg = ?, 
+                    jours_sans_manger = ?, 
+                    pourcentage_perte_poids = ? 
+                WHERE id = ?"; 
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$nom, $poidsMin, $poidsMax, $prixVenteKg, $jour, $pourcentage, $id]);
+        return $stmt->rowCount();
     }
 
 }
