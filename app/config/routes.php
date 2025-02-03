137d6ed20ecf99ac52b3fbc3ff5a2f17bfbcaac6
NyEjaBoum <razanatsimbanyeja@gmail.com>
2025-02-03 18:58:48 +0300

    t

--- app/config/routes.php
+++ app/config/routes.php
@@ -13,7 +13,6 @@ use flight\net\Router;
 
 
 $userController = new userController();
-$hab = new HabitationController();
 
 
 $router->get('/',function(){
@@ -41,37 +40,13 @@ $router->get('/admin',function(){
 	Flight::render('admin');
 });
 
-//$router->get('/list',[ $hab, 'list' ]);
-
-//$router->get('/details',[ $hab, 'getById' ]);
 
 
-$router->get('/deleteHabit',[ $hab, 'delete' ]);
-
-$router->post('/addHabit',[ $hab, 'ajoutHabitation' ]);
-
-$router->post('/modifHabit',[ $hab, 'modifierHabitation' ]);
 
 $router->post('/login',[ $userController, 'login' ]);
 
 $router->post('/signin',[ $userController, 'signin' ]);
 
-$router->post('/reservation',[ $hab, 'reserverHabitation' ]);
-
-
-
-$router->get('/details', function() use ($hab) {
-    $hab->getById('details');
-});
-
-
-$router->get('/ajout', function() use ($hab) {
-    $hab->getAllTypeForPage('ajoutHabit');
-});
-
-$router->get('/modif', function() use ($hab) {
-    $hab->forModifPage('modifHabit');
-});
 
 //$router->get('/users/@id:[0-9]', [ $Api_Example_Controller, 'getUser' ]);
 
