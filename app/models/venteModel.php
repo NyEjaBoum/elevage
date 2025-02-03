4d08554340f7798fcfd3e2abf5e49fa7e2abc03d
NyEjaBoum <razanatsimbanyeja@gmail.com>
2025-02-03 23:37:29 +0300

    desole
    desole

--- app/models/venteModel.php
+++ app/models/venteModel.php
@@ -11,10 +11,39 @@ class venteModel {
         $this->conn = $db;
     }
 
-    public function venteAnimal($idAnimal,$id_utilisateur){
+    
+
+    public function getPoidsMinAnimal($id){
+        $sql = "SELECT poids_minimal_vente as poids FROM ELEVAGE_TYPEANIMAL WHERE ID = $id";
+        $stmt = $this->db->query($sql);
+        $result = $stmt->fetch();
+        return $result['poids'];
+    }
+
+    public function getPrixParKgAnimal($id){
+        $sql = "SELECT prix_vente_kg as prix FROM ELEVAGE_TYPEANIMAL WHERE ID = $id";
+        $stmt = $this->db->query($sql);
+        $result = $stmt->fetch();
+        return $result['prix'];
+    }
+
+    public function getPrixAnimal($id){
+        $poids = $this->getPoidsMinAnimal($id);
+        $prixParKg = $this->getPrixAnimal($id);
+        $prix = $poids*$prixParKg;
+        return $prix;
+    }
+
+    public function venteAnimal($idAnimal,$id_utilisateur,$date,$quantite){
         // transcation
         //table animal
-        $sql1 = "INSERT INTO elevage_transactionAnimal (type_transaction,montant,date_transaction,utilisateur_id,animal_id,quantite)"
+        $montant = $this->getPrixAnimal($id);
+        $poids = $this->getPoidsMinAnimal($idAnimal);
+        $sql1 = "INSERT INTO elevage_transactionAnimal (type_transaction,montant,date_transaction,utilisateur_id,animal_id,quantite) 
+        VALUES ('vente',$montant,'$date',$id_utilisateur,$idAnimal,$quantite)";
+
+        $sql2 = "INSERT INTO ELEVAGE_ANIMAL(image,nom,poids_actuel,date_achat,utilisateur_id,type_animal_id,quantite) VALUES 
+        ('$image','$nom',)";
     }
 }
 ?>
--- sql/insert.sql
+++ sql/insert.sql
@@ -13,19 +13,19 @@ INSERT INTO elevage_typeAnimal (nom, poids_minimal_vente, poids_maximal, prix_ve
 ('Poule', 1.0, 2.0, 12.00, 2, 1.0);
 
 -- Insérer des animaux dans la table elevage_animal
-INSERT INTO elevage_animal (image, nom, poids_actuel, date_achat, est_vivant, utilisateur_id, type_animal_id,quantite) VALUES
-('assets/images/vache1.jpg', 'Vache Tachetée', 45.5, '2025-01-01', TRUE, 1, 1,1),
-('assets/images/vache2.jpg', 'Vache Brune', 47.0, '2025-01-02', TRUE, 1, 1,5),
-('assets/images/cochon1.jpg', 'Cochon Gris', 22.1, '2025-01-03', TRUE, 1, 2,3),
-('assets/images/cochon2.jpg', 'Cochon Rose', 24.5, '2025-01-04', TRUE, 1, 2,4),
-('assets/images/lapin1.jpg', 'Lapin Beige', 1.8, '2025-01-05', TRUE, 1, 3,1),
-('assets/images/lapin2.jpg', 'Lapin Blanc', 2.0, '2025-01-06', TRUE, 1, 3,1),
-('assets/images/mouton1.jpg', 'Mouton Blanc', 20.5, '2025-01-07', TRUE, 1, 4,2),
-('assets/images/mouton2.jpg', 'Mouton Marron', 18.3, '2025-01-08', TRUE, 1, 4,4),
-('assets/images/cheval1.jpg', 'Cheval Noir', 35.2, '2025-01-09', TRUE, 1, 5,1),
-('assets/images/cheval2.jpg', 'Cheval Blanc', 37.8, '2025-01-10', TRUE, 1, 5,1),
-('assets/images/poule1.jpg', 'Poule Rousse', 2.3, '2025-01-11', TRUE, 1, 6,2),
-('assets/images/poule2.jpg', 'Poule Blanche', 2.8, '2025-01-12', TRUE, 1, 6,3);
+INSERT INTO elevage_animal (nom, poids_actuel, date_achat, est_vivant, utilisateur_id, type_animal_id,quantite) VALUES
+('Vache Tachetée', 45.5, '2025-01-01', TRUE, 1, 1,1),
+('Vache Brune', 47.0, '2025-01-02', TRUE, 1, 1,5),
+('Cochon Gris', 22.1, '2025-01-03', TRUE, 1, 2,3),
+('Cochon Rose', 24.5, '2025-01-04', TRUE, 1, 2,4),
+('Lapin Beige', 1.8, '2025-01-05', TRUE, 1, 3,1),
+('Lapin Blanc', 2.0, '2025-01-06', TRUE, 1, 3,1),
+('Mouton Blanc', 20.5, '2025-01-07', TRUE, 1, 4,2),
+('Mouton Marron', 18.3, '2025-01-08', TRUE, 1, 4,4),
+('Cheval Noir', 35.2, '2025-01-09', TRUE, 1, 5,1),
+('Cheval Blanc', 37.8, '2025-01-10', TRUE, 1, 5,1),
+('Poule Rousse', 2.3, '2025-01-11', TRUE, 1, 6,2),
+('Poule Blanche', 2.8, '2025-01-12', TRUE, 1, 6,3);
 
 -- Insérer des transactions dans la table elevage_transactionAnimal
 INSERT INTO elevage_transactionAnimal (type_transaction, montant, date_transaction, utilisateur_id, animal_id, quantite) VALUES
